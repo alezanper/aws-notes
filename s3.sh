edd4c787d63966c60cb5f46ca4ff0b44d8be8409
Alexander Benavides <fabenavideso@gmail.com>
2021-03-24 20:59:02 -0500

    [updated] s3 commands

--- s3.sh
+++ s3.sh
@@ -1,7 +1,7 @@
 # List S3 buckets
 aws s3 ls
 
-# List S3 files
+# List files on S3 bucket
 aws s3 ls s3://bucketname
 
 # Create a bucket
@@ -10,5 +10,5 @@ aws s3 mb s3://bucketnametocreate
 # Remove a bucket
 aws s3 rb s3://buckettodelete
 
-# Copy a file from bucket
+# Copy a file from bucket to console
 aws s3 cp s3://bucketorigin/image.jpg image.jpg
